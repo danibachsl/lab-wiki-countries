c5ad38499db8673bba64e1ddae4227767d725848
Dani BL <danibachsl@protonmail.com>
2022-07-16 12:42:41 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-// src/App.js
-import "./App.css";
+import './App.css';
+import Navbar from './components/Navbar';
+import CountriesList from './components/CountriesList';
+import { Routes, Route } from 'react-router-dom';
+import CountryDetails from './components/CountryDetails';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+
+      <div style={{display: "flex"}}>
+        <CountriesList />
+
+        <Routes>
+          <Route path="/:alpha3Code" element={<CountryDetails />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
+
 export default App;
